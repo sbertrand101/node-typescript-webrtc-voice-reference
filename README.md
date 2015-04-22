cae5ece97d67aebe92134dfc470704106778d590
Andrey Belchikov <belchikovs@gmail.com>
2015-04-22 04:17:01 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+#nodejs-reference-app-voice
+
+
+## Install
+Before run them fill config file (config.json) with right values:
+
+`domain` - domain name (it will be created by the app if need),
+
+`catapultUserId`, `catapultApiToken`, `catapultApiSecret` - auth data for Catapult API (to search and reserve a phone number, etc)
+
+`baseUrl` - base url of this site for external access (like http://<your-site>.heroku.com),
+
+After that run `npm install`  to install dependencies.
+
+You can run this demo  like `node index.js` on local machine if you have ability to handle external requests or use any external hosting.
+
+## Deploy on Heroku
+
+Create account on [Heroku](https://www.heroku.com/) and install [Heroku Toolbel](https://devcenter.heroku.com/articles/getting-started-with-nodejs#set-up) if need.
+
+Open `config.json` and fill it with valid values (except `baseUrl`).
+
+Commit your changes.
+
+```
+git add .
+git commit -a -m "Deployment"
+```
+
+Run `heroku create` to create new app on Heroku and link it with current project.
+
+Change option `baseUrl` in `config.json` by assigned by Heroku value (something like http://XXXX-XXXXXX-XXXX.heroku.com). Commit your changes by `git commit -a`. 
+
+Run `git push heroku master` to deploy this project.
+
+Run `heroku open` to see home page of the app in the browser
+
+
+## Http routes
+
+```
+GET /users/{userName} with json response
+
+PUT /users/{userName}
+
+DELETE /users/{userName}
+
+POST /users with required json payload {"userName": "", "password": "" }  and with json response (register an user)
+
+POST /users/{userName}/callback with json payload (handle Catapult call events)
+```
---
+++ index.js
@@ -0,0 +1,295 @@
+var Joi = require("joi");
+var Hapi = require("hapi");
+var boom = require("boom");
+var path = require("path");
+var catapult = require("node-bandwidth");
+var config = require("./config.json");
+var thenifyAll = require("thenify-all");
+var randomstring = require("randomstring");
+var fs = require("mz/fs");
+var debug = require("debug")("voice");
+
+var server = new Hapi.Server(); //server instance
+
+catapult.Client.globalOptions.userId = config.catapultUserId;
+catapult.Client.globalOptions.apiToken = config.catapultApiToken;
+catapult.Client.globalOptions.apiSecret = config.catapultApiSecret;
+
+var Application = thenifyAll(catapult.Application);
+var AvailableNumber = thenifyAll(catapult.AvailableNumber);
+var PhoneNumber = thenifyAll(catapult.PhoneNumber);
+var Domain = thenifyAll(catapult.Domain);
+var Call = thenifyAll(catapult.Call);
+var Bridge = thenifyAll(catapult.Bridge);
+catapult.PhoneNumber.prototype = thenifyAll(catapult.PhoneNumber.prototype);
+catapult.Domain.prototype = thenifyAll(catapult.Domain.prototype);
+catapult.Call.prototype = thenifyAll(catapult.Call.prototype);
+catapult.Bridge.prototype = thenifyAll(catapult.Bridge.prototype);
+thenifyAll.withCallback(server, server, ["start"]);
+
+server.connection({ port: process.env.PORT || 3000, host: process.env.HOST || "0.0.0.0" });
+
+var usersPath = path.join(__dirname, "users.json");
+var users = {};
+var domain = null;
+var bridges = {};
+
+function saveUsers(){
+  return fs.writeFile(usersPath, JSON.stringify(users));
+}
+
+function formatUser(user){
+  var k, u = {};
+  for(k in user){
+    if(k === "password" || k === "application") continue;
+    u[k] = user[k];
+  }
+  return u;
+}
+
+function processEvent(ev, user){
+  switch(ev.eventType){
+    case "incomingcall":
+      var callbackUrl = config.baseUrl + "/users/" + encodeURIComponent(user.userName) + "/callback";
+      if(user.phoneNumber === ev.to){
+        //incoming call
+        debug("Handle incoming call: call to sip %s", user.endpoint.sipUri);
+        return Call.create({
+          from: user.phoneNumber,
+          to: user.endpoint.sipUri,
+          callbackUrl: callbackUrl,
+          tag: ev.callId
+        });
+      }
+      if(user.endpoint.sipUri.indexOf(ev.from.trim()) >= 0){
+        //outgoing call
+        debug("Handle outgoing call: call to  %s", ev.to);
+        return Call.create({
+          from: user.phoneNumber,
+          to: ev.to,
+          callbackUrl: callbackUrl,
+          tag: ev.callId
+        });
+      }
+      break;
+    case "answer":
+      if(!ev.tag){
+        return Promise.resolve();
+      }
+      return Call.get(ev.tag)
+      .then(function(call){
+        if(call.bridgeId){
+          return Promise.reject();
+        }
+        return (call.state === "active")?Promise.resolve():call.answerOnIncoming();
+      })
+      .then(function(){
+        return Bridge.create({
+          callIds: [ev.callId, ev.tag],
+          bridgeAudio: true
+        });
+      })
+      .then(function(bridge){
+        bridges[ev.callId] = bridge.id;
+        bridges[ev.tag] = bridge.id;
+      });
+      break;
+    case "hangup":
+      var bridgeId = bridges[ev.callId];
+      if(!bridgeId){
+        return Promise.resolve();
+      }
+      return Bridge.get(bridgeId)
+      .then(function(bridge){
+        return bridge.getCalls();
+      })
+      .then(function(calls){
+        return Promise.all(calls.map(function(c){
+          delete bridges[c.id];
+          if(c.state === "active"){
+            debug("Hangup another call");
+            return c.hangUp();
+          }
+        }));
+      });
+      break;
+  }
+}
+
+// Routes
+
+//POST /users
+server.route({
+  path: "/users",
+  method: "POST",
+  handler: function(req, reply){
+    var user = req.payload;
+    Application.create({
+      name: user.userName,
+      incomingCallUrl: config.baseUrl + "/users/" + encodeURIComponent(user.userName) + "/callback",
+      autoAnswer: true
+    })
+    .then(function(application){
+      user.application = application;
+      return AvailableNumber.searchLocal({state: "NC", quantity: 1});
+    })
+    .then(function(numbers){
+      user.phoneNumber = numbers[0].number;
+      return PhoneNumber.create({number: user.phoneNumber, applicationId: user.application.id});
+    })
+    .then(function(){
+      return domain.createEndPoint({
+        name: "uep-" + randomstring.generate(12),
+        description: "Sandbox created Endpoint for user " + user.userName,
+        domainId: domain.id,
+        applicationId: user.application.id,
+        enabled: true,
+        credentials: {password: user.password}
+      });
+    })
+    .then(function(endpoint){
+      user.endpoint = endpoint;
+      delete user.application.client;
+      delete user.endpoint.client;
+      users[user.userName] = user;
+      return saveUsers();
+    })
+    .then(function(){
+      reply(formatUser(user)).created(config.baseUrl + "/users/" + encodeURIComponent(user.userName));
+    })
+    .catch(function(err){
+      reply(err);
+    });
+  },
+  config: {
+    validate:{
+      payload: Joi.object().keys({
+        userName: Joi.string().required(),
+        password: Joi.string().required()
+      })
+    }
+  }
+});
+
+//GET /users/{userName}
+server.route({
+  path: "/users/{userName}",
+  method: "GET",
+  handler: function(req, reply){
+    var user = users[req.params.userName];
+    if(user){
+      return reply(formatUser(user));
+    }
+    reply(boom.notFound());
+  }
+});
+
+
+//PUT /users/{userName}
+server.route({
+  path: "/users/{userName}",
+  method: "PUT",
+  handler: function(req, reply){
+    var k, body = req.payload || {};
+    var user = users[req.params.userName];
+    if(user){
+      for(k in body){
+        user[k] = body[k];
+      }
+      return saveUsers().then(function(){
+        reply("");
+      }, function(err){
+        reply(err);
+      });
+    }
+    reply(boom.notFound());
+  }
+});
+
+
+//DELETE /users/{userName}
+server.route({
+  path: "/users/{userName}",
+  method: "DELETE",
+  handler: function(req, reply){
+    var user = users[req.params.userName];
+    if(user){
+      var phoneNumber = user.phoneNumber;
+      delete users[req.params.userName];
+      return saveUsers()
+      .then(function(){
+        return PhoneNumber.get(phoneNumber);
+      })
+      .then(function(number){
+        if(number){
+          return number.delete();
+        }
+      })
+      .then(function(){
+        reply("");
+      }, function(err){
+        reply(err);
+      });
+    }
+    reply(boom.notFound());
+  }
+});
+
+
+//POST /users/{userName}/callback
+server.route({
+  path: "/users/{userName}/callback",
+  method: "POST",
+  handler: function(req, reply){
+    var user = users[req.params.userName];
+    if(user){
+      var ev = req.payload;
+      debug(ev);
+      processEvent(ev, user).then(function(){
+        reply("");
+      }, function(err){
+        console.error("Callback error:" + err.message);
+        reply("");
+      });
+    }
+    reply(boom.notFound());
+  }
+});
+
+fs.exists(usersPath)
+.then(function(exists){
+  if(exists){
+    debug("Read users data from json file");
+    return fs.readFile(usersPath);
+  }
+  return "{}";
+})
+.then(function(json){
+  debug("Parse users json data");
+  users = JSON.parse(json);
+  debug("Loaded %d users", Object.keys(users).length);
+  return Domain.list();
+})
+.then(function(domains){
+  var dm = domains.filter(function(d){
+    return d.name === config.domain;
+  })[0];
+  if(dm){
+    debug("Using existing domain %s", dm.name);
+    return dm;
+  }
+  debug("Creating a domain %s", config.domain);
+  return Domain.create({name: config.domain});
+})
+.then(function(d){
+  domain = d;
+  debug("Start the server");
+  return server.start();
+})
+.then(function(){
+  console.log("Server running at:", server.info.uri);
+})
+.catch(function(err){
+    return console.error(err);
+});
+
