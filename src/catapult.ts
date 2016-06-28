7710a50e947acc923f43ec9de0ae368532b5c3e0
Andrey Belchikov <belchikovs@gmail.com>
2016-06-28 15:47:57 +0300

    Added stubs for all catapult actions

--- src/catapult.ts
+++ src/catapult.ts
@@ -48,12 +48,71 @@ export interface IMediaFile {
 	contentType: string;
 }
 
-class CatapultApi extends ICatapultApi {
+class CatapultApi implements ICatapultApi {
+	constructor(private userId: string, private apiToken: string, private apiSecret: string) {
+	}
+	createPhoneNumber(areaCode: string): Promise<string> {
+		throw new Error('Not implemented yet');
+	}
+	createSIPAccount(): Promise<ISIPAccount> {
+		throw new Error('Not implemented yet');
+	}
 
+	createSIPAuthToken(endpointId: string): Promise<ISIPAuthToken> {
+		throw new Error('Not implemented yet');
+	}
+
+	createBridge(data: any): Promise<string> {
+		throw new Error('Not implemented yet');
+	}
+
+	createCall(data: any): Promise<string> {
+		throw new Error('Not implemented yet');
+	}
+
+	createGather(data: any): Promise<string> {
+		throw new Error('Not implemented yet');
+	}
+
+	updateCall(callId: string, data: any): Promise<string> {
+		throw new Error('Not implemented yet');
+	}
+
+	stopPlayAudioToCall(callId: string): Promise<void> {
+		throw new Error('Not implemented yet');
+	}
+
+	playAudioToCall(callId: string, url: string, loop: boolean, tag: string): Promise<void> {
+		throw new Error('Not implemented yet');
+	}
+
+	transferCall(to: string, callerId: string): Promise<string> {
+		throw new Error('Not implemented yet');
+	}
+
+	speakSentenceToCall(callId: string, text: string, tag: string): Promise<void> {
+		throw new Error('Not implemented yet');
+	}
+
+	getCall(callId: string): Promise<ICall> {
+		throw new Error('Not implemented yet');
+	}
+
+	getRecording(recordingId: string): Promise<IRecording> {
+		throw new Error('Not implemented yet');
+	}
+
+	hangup(callId: string): Promise<void> {
+		throw new Error('Not implemented yet');
+	}
+
+	downloadMediaFile(name: string): Promise<IMediaFile> {
+		throw new Error('Not implemented yet');
+	}
 }
 
 export const catapultMiddleware = async (ctx: IContext, next: Function) => {
-	ctx.api = <ICatapultApi>new CatapultApi();
+	ctx.api = <ICatapultApi>new CatapultApi(process.env.CATAPULT_USER_ID, process.env.CATAPULT_API_TOKEN, process.env.CATAPULT_API_SECRET);
 	await next();
 };
 
